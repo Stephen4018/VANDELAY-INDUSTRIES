3ba3ce313eb9f272b58427eb6bb98cfc8070ddb3
stephen4018 <stephenohaeri4018@gmail.com>
2022-11-28 01:32:11 +0100

    dashboard UI setup

--- src/component/AsideComp.js
+++ src/component/AsideComp.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
 
 const AsideComp = () => {
   return (
-    <div className="aside-container">
-      <img src="logos.png" alt="" />
+    <div className="aside-section">
+      <div className="aside-section-inner">
+        <div className="logo">
+          <img src="logos.png" alt="" />
+        </div>
+        <div className="line"></div>
+        <section className="aside-inner-top">
+          <div className="dashboard">
+            <div className="dashboard-text-icon">
+              <FaHome />
+            </div>
+            <p>Dashboard</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
